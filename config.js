6e77f498191fef3e0de31199e337cbd23cf3fba7
Surya <suryajoshi617@gmail.com>
2025-07-21 15:23:40 +0530

    dlkf

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 export const REDIRECT_URL='https://canva-redirect.onrender.com'
-export const BASE_URL='https://canva-testing-backend.vercel.app'
+export const BASE_URL='https://40klr9s0-4000.inc1.devtunnels.ms'
